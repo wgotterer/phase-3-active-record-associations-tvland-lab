520d268289908f829b5533660e7b7f14f345d7f8
William Gotterer <william.gotterer@gmail.com>
2021-11-07 11:10:20 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,10 @@
+require "pry"
+
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
 
-  def network
- self.network.call_letters
-  end
 
   def actors_list
     self.actors.map do |actor|
